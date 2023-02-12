4eb5c94db8c730a9654cfb2b1383143e339407e3
Erik Blaha <erik.blaha4@gmail.com>
2023-02-12 22:09:53 +0100

    improves sort and design

--- JavascriptANDjson/script.js
+++ JavascriptANDjson/script.js
@@ -1,98 +1,100 @@
+
+// change the color
+function setIncreaseColor(importanceTextElements, weeklyTasks, i) {
+  importanceTextElements[i].innerHTML = weeklyTasks[i].importance;
+
+  let color = '';
+
+  if (
+    weeklyTasks[i].importance >= 0 &&
+    weeklyTasks[i].importance <= 1
+  ) {
+    color = 'btn-success';
+  } else if (
+    weeklyTasks[i].importance >= 2 &&
+    weeklyTasks[i].importance <= 3
+  ) {
+    color = 'btn-warning';
+  } else if (
+    weeklyTasks[i].importance >= 4
+  ) {
+    color = 'btn-danger';
+  }
+
+  importanceTextElements[i].className = `btn ${color} importance-number`;
+}
+
+function displayTasks(weeklyTasks) {
+  document.getElementById('result').innerHTML = '';
+  for (let val of weeklyTasks) {
+    document.getElementById('result').innerHTML += `
+    <div class="mb-2 col-lg-4 col-md-6 col-sm-12">
+      <div class="card shadow-lg bg-body">
+        <img src="${val.pic}" class="card-img-top" alt="${val.taskName}">
+        <div class="card-body">
+          <h5 class="card-title">${val.taskName}</h5>
+          <p class="card-text">${val.description} </p>
+          <p> ${val.duration} </p>
+          <p> ${val.deadline} <i class="fa-solid fa-calendar-days"></i></p>
+          <p class="importance">
+            <span class="increase-importance"><i class="fa-solid fa-triangle-exclamation"></i></span>
+            Importance: <span class="importance-number">${val.importance}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+      `;
+  }
+
+  // increase button 
+  let increaseImportanceButtons = document.getElementsByClassName(
+    'increase-importance'
+  );
+
+  const importanceTextElements =
+    document.querySelectorAll('.importance > .importance-number');
+
+  for (let i = 0; i < increaseImportanceButtons.length; i++) {
+    setIncreaseColor(importanceTextElements, weeklyTasks, i);
+
+    increaseImportanceButtons[i].addEventListener('click', function () {
+      weeklyTasks[i].importance++;
+
+      setIncreaseColor(importanceTextElements, weeklyTasks, i);
+    });
+  }
+}
+
 fetch('/JavascriptAndjson/data.json')
   .then((response) => response.json())
   .then((data) => {
     let weeklyTasks = data.tasks;
-    for (let val of weeklyTasks) {
-      document.getElementById('result').innerHTML += `
-      <div class="mb-2 col-lg-4 col-md-6 col-sm-12">
-        <div class="card" style="width: 18rem;">
-          <img src="${val.pic}" class="card-img-top" alt="${val.taskName}">
-          <div class="card-body">
-            <h5 class="card-title">${val.taskName}</h5>
-            <p class="card-text">${val.description} </p>
-            <p> ${val.duration} </p>
-            <p class="importance">Importance: <span>${val.importance}</span></p>
-            <button class="btn btn-info increase-importance">Increase </button>
-          </div>
-        </div>
-      </div>
-        `;
-
-      // increase button and change the color
-
-      let increaseImportanceButtons = document.getElementsByClassName(
-        'increase-importance'
-      );
-
-      const importanceTextElements =
-        document.querySelectorAll('.importance > span');
-
-      for (let i = 0; i < increaseImportanceButtons.length; i++) {
-        increaseImportanceButtons[i].addEventListener('click', function () {
-          weeklyTasks[i].importance++;
-
-          importanceTextElements[i].innerHTML = weeklyTasks[i].importance;
-
-          let color = '';
-
-          if (
-            weeklyTasks[i].importance >= 0 &&
-            weeklyTasks[i].importance <= 1
-          ) {
-            color = 'btn-success';
-          } else if (
-            weeklyTasks[i].importance >= 2 &&
-            weeklyTasks[i].importance <= 3
-          ) {
-            color = 'btn-warning';
-          } else if (
-            weeklyTasks[i].importance >= 4 &&
-            weeklyTasks[i].importance <= Infinity
-          ) {
-            color = 'btn-danger';
-          }
-
-          importanceTextElements[i].className = `btn ${color}`;
-        });
-      }
-    }
+    displayTasks(weeklyTasks);
 
     // how to sort
 
     let sortButton = document.getElementById('sortButton');
 
+    let sortDirection;
     sortButton.addEventListener('click', function () {
-      weeklyTasks.sort(function (a, b) {
-        return a.importance - b.importance;
+      const sortButton = document.querySelectorAll(".sortButton > i")[0];
+      if (sortDirection === "asc") {
+        sortDirection = "desc";
+        sortButton.classList = "fa-solid fa-arrow-up-wide-short";
+      } else {
+        sortDirection = "asc";
+        sortButton.classList = "fa-solid fa-arrow-down-wide-short";
+      }
+
+      const sortedWeeklyTasks = weeklyTasks.sort(function (a, b) {
+        if (sortDirection === "desc") {
+          return a.importance - b.importance;
+        }
+        return b.importance - a.importance;
       });
-      displayTasks();
+
+      displayTasks(sortedWeeklyTasks);
     });
 
-    function displayTasks() {
-      document.getElementById('result').innerHTML = '';
-      for (let val of weeklyTasks) {
-        document.getElementById('result').innerHTML += `
-        <div class="mb-2">
-          <div class="card" style="width: 18rem;">
-            <img src="${val.pic}" class="card-img-top" alt="${val.taskName}">
-            <div class="card-body">
-              <h5 class="card-title">${val.taskName}</h5>
-              <p class="card-text">${val.description} </p>
-              <p> ${val.duration} </p>
-              <p class="importance">Importance: <span>${val.importance}</span></p>
-              <button class="btn btn-secondary increase-importance">Status</button>
-            </div>
-          </div>
-        </div>
-        `;
-      }
-      let btns = document.getElementsByClassName('likeBtn');
-      for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', function () {
-          weeklyTasks[i].importance++;
-          document.getElementsByClassName('likes')[i].innerHTML =
-            weeklyTasks[i].importance;
-        });
-      }
-    }
+    
   });
